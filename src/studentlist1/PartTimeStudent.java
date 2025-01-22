a12d9d8e025c09b136b0f841a9e2b3520ecac8fe
Amanpreet kaur <kau17785@sheridancollege.ca>
2025-01-22 12:09:58 -0500

    added class part time student

---
+++ src/studentlist1/PartTimeStudent.java
@@ -0,0 +1,18 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist1;
+
+/**
+ *
+ * @author kaura
+ */
+
+public class PartTimeStudent extends Student{
+    public void getNumCourse(){
+       System.out.println("This is a new class for the branch");
+    }
+    
+}
