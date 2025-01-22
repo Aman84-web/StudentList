6a64d6ded65826a0a78f58b86e957b76ffbbc5a5
Amanpreet kaur <kau17785@sheridancollege.ca>
2025-01-22 10:42:10 -0500

    Initial Commit

---
+++ src/studentlist1/Student.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist1;
+
+/**
+ *
+ * @author fagun at 12/05/2024
+ */
+public class Student {
+    private String name;
+    private String studentID;
+    private String address;
+private String lastname;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+   
+}
